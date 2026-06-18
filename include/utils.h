5243e68424286bea398731d0c613c523f57ae704
naipret <ngvphu235e@gmail.com>
2026-06-18 14:19:11 +0700

    feat(utils): update header signatures for safe input helper functions

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-int get_safe_int(const char *prompt, const int *out_value);
-int get_safe_float(const char *prompt, const float *out_value);
-void get_safe_string(const char *prompt, const char *out_str, int max_len);
+int get_safe_int(const char *prompt, int *out_value);
+int get_safe_float(const char *prompt, float *out_value);
+void get_safe_string(const char *prompt, char *out_str, int max_len);
 void clear_input_buffer(void);
-void to_lowercase(const char *str);
+void to_lowercase(char *str);
 
 #endif /* UTILS_H */
