0502380b7f5bc0b27095e338f7bda7485c2700e8
naipret <ngvphu235e@gmail.com>
2026-06-18 14:42:38 +0700

    feat(admin): update header to declare run_admin_registration_wizard

--- include/admin.h
+++ include/admin.h
@@ -6,5 +6,6 @@
 int is_admin_setup_complete(void);
 int setup_admin_credentials(const char *username, const char *password);
 int verify_admin_login(const char *username, const char *password);
+void run_admin_registration_wizard(void);
 
 #endif /* ADMIN_H */
